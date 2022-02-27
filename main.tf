41678809c8d7ff55e1b8ddf8b0762e5fe6d239c6
vjpartha <96643117+vjpartha@users.noreply.github.com>
2022-02-27 15:55:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         # profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "vijayinfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+	instance_type = "t3.micro"
 }
