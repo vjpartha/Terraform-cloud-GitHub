f1344d9de121714480945ec4adc4f5a10153c142
vjpartha <96643117+vjpartha@users.noreply.github.com>
2022-02-27 15:37:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.1.5" // added latest version
+    required_version = "~> 1.1.3" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
